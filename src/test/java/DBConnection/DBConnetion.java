bb37210cdfa477c3307627995cfae4a5373429d7
Jose Miguel Hernandez Neri <j.miguel.hn@hotmail.com>
2019-07-26 10:33:33 -0500

    Db connection update

--- src/test/java/DBConnection/DBConnetion.java
+++ src/test/java/DBConnection/DBConnetion.java
@@ -1,4 +1,57 @@
 package DBConnection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBConnetion {
+    /*public void hello(){
+        System.out.println("HEllo");
+    }
+    public void hell(){
+        this.hello();
+        this.hello();
+    }*/
+
+    final String DRIVER = "com.mysql.jdbc.Driver";
+    final String DB_PATH = "jdbc:mysql://localhost/java_test";
+    String user = "root";
+    String pass = "admin";
+    Connection connection = null;
+    Statement statement = null;
+
+    public DBConnetion(){
+
+    }
+    public DBConnetion(String user, String pass){
+        this.user = user;
+        this.pass = pass;
+    }
+    public void connect() throws SQLException, Exception {
+        Class.forName(DRIVER);
+        System.out.println("Connecting...");
+        connection = DriverManager.getConnection(DB_PATH, user, pass);
+        statement = connection.createStatement();
+
+    }
+
+    public void closeConnection() throws SQLException, Exception{
+        statement.close();
+        connection.close();
+    }
+
+    public Statement getStatement(){
+        return statement;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+    public void setCredentials(String user, String pass){
+        this.user = user;
+        this.pass = pass;
+    }
+
+
+
 }
